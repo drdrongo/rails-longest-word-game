177423ebdbeae2aab41a425e9a2f15ece4c1ff20
Hayato Clarke <hayatoclarke@gmail.com>
2020-06-25 22:12:58 +0900

    added shuffle button

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,23 +3,25 @@ require 'open-uri'
 
 class GamesController < ApplicationController
   def new
-
     vowels = %w[a e i o u]
     consonants = %w[b c d f g h j k l m n p q r s t v w x y z]
     @letters = (vowels.sample(3) + consonants.sample(6)).shuffle
   end
 
   def score
+    session[:score] = 0
+
     response = JSON.parse(open("https://wagon-dictionary.herokuapp.com/#{params[:word]}").read)
     array_diff = array_difference(params[:word].split(''), params[:letters].split(' '))
 
     if array_diff.length != 0
-      @message = "Sorry, but '#{params[:word]}' can't be build out of '#{params[:letters].upcase}'"
+      @message = "Sorry, but '#{params[:word]}' can't be built out of '#{params[:letters].upcase}'"
     elsif !response['found']
       @message = "Sorry, but '#{params[:word]}' does not seem to be a valid English word..."
     else
       @message = "Congratulations! '#{params[:word]}' is a valid English word!"
-      @session_score = calculate_score(params[:word])
+      display_words
+      calculate_score(params[:word])
     end
   end
 
@@ -34,12 +36,19 @@ class GamesController < ApplicationController
   end
 
   def calculate_score(word)
-    score = 1
-    counter = 0
-    word.split('')[1..-1].each do |letter|
-      score += counter
-      counter += 1
+    session[:longest_word] = '' if session[:longest_word] == nil
+    session[:total_score] = 0 if session[:total_score] == nil
+
+    session[:score] = word.length ** 2
+    session[:total_score] += session[:score]
+    if word.length >= session[:longest_word].length
+      session[:longest_word] = word
     end
-    return score
+  end
+
+  def display_words
+    session[:words_array] = [] if session[:words_array] == nil
+    session[:words_array] << params[:word]
+    session[:words_array] = session[:words_array].uniq.sort_by(&:length)[0..9].reverse
   end
 end
