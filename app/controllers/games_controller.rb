6112e0c07ec36d348ef9e48149c221a42a268666
Hayato Clarke <hayatoclarke@gmail.com>
2020-06-25 20:13:54 +0900

    did up to sessions and points calculation

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,45 @@
+require 'json'
+require 'open-uri'
+
+class GamesController < ApplicationController
+  def new
+
+    vowels = %w[a e i o u]
+    consonants = %w[b c d f g h j k l m n p q r s t v w x y z]
+    @letters = (vowels.sample(3) + consonants.sample(6)).shuffle
+  end
+
+  def score
+    response = JSON.parse(open("https://wagon-dictionary.herokuapp.com/#{params[:word]}").read)
+    array_diff = array_difference(params[:word].split(''), params[:letters].split(' '))
+
+    if array_diff.length != 0
+      @message = "Sorry, but '#{params[:word]}' can't be build out of '#{params[:letters].upcase}'"
+    elsif !response['found']
+      @message = "Sorry, but '#{params[:word]}' does not seem to be a valid English word..."
+    else
+      @message = "Congratulations! '#{params[:word]}' is a valid English word!"
+      @session_score = calculate_score(params[:word])
+    end
+  end
+
+  def array_difference(x, y)
+    ret = x.dup
+    y.each do |element|
+      if index = ret.index(element)
+        ret.delete_at(index)
+      end
+    end
+    ret
+  end
+
+  def calculate_score(word)
+    score = 1
+    counter = 0
+    word.split('')[1..-1].each do |letter|
+      score += counter
+      counter += 1
+    end
+    return score
+  end
+end
